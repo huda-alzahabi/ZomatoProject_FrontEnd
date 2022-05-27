c5fc39db4f32a5e274ddb805b5a877805d9d6b76
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-27 10:56:22 +0300

    Added signup and styled it

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", showSignUp, false);
+
+function showSignUp() {
+    document.getElementById("signup").addEventListener("click", function() {
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("signupBox").style.display = "block";
+    });
+    document.getElementById("back").addEventListener("click", function() {
+        document.getElementById("signupBox").style.display = "none";
+        document.getElementById("loginBox").style.display = "block";
+    });
+}
