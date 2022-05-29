cc21e6b8ee812e20c091bce556f3edd2af24afe7
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-29 16:24:39 +0300

    tried to fix connectin using axios

--- scripts/login.js
+++ scripts/login.js
@@ -1,28 +1,54 @@
 window.addEventListener("load", showSignUp, false);
 
 function showSignUp() {
-    document.getElementById("signup").addEventListener("click", function() {
-        document.getElementById("loginBox").style.display = "none";
-        document.getElementById("signupBox").style.display = "block";
+    let first_signup = document.getElementById("signup");
+    let loginbox = document.getElementById("loginBox");
+    let signupbox = document.getElementById("signupBox");
+    let backbtn = document.getElementById("back");
+    let submit_signup = document.getElementById("s");
+    let username = document.getElementById("full_name").value;
+    let email = document.getElementById("signup_email").value;
+    let password = document.getElementById("signup_pass").value;
+
+    first_signup.addEventListener("click", function() {
+        loginbox.style.display = "none";
+        signupbox.style.display = "block";
     });
-    document.getElementById("back").addEventListener("click", function() {
-        document.getElementById("signupBox").style.display = "none";
-        document.getElementById("loginBox").style.display = "block";
+    backbtn.addEventListener("click", function() {
+        signupbox.style.display = "none";
+        loginbox.style.display = "block";
     });
 
-    document.getElementById("s").addEventListener("click", function() {
-        let data = new FormData();
+    submit_signup.addEventListener("click", function() {
+        console.log(document.getElementById("full_name").value);
+        console.log(document.getElementById("signup_email").value);
+        console.log(document.getElementById("signup_pass").value);
+        let vdata = new FormData();
+        vdata.append("full_name", document.getElementById("full_name").value);
+        vdata.append("email", document.getElementById("signup_email").value);
+        vdata.append("password", document.getElementById("signup_pass").value);
+        console.log(vdata);
 
-        data.append("signup_email", "Test@hotmail.com");
-        data.append("signup_pass", "9898");
-        data.append("full_name", "Huda");
+        //     axios
+        //         .post("http://localhost:8080/ZomatoProject_BackEnd/signup.php", data)
+        //         .then(function(response) {
+        //             console.log(response);
+        //             location.href = "../main_page.html";
+        //         });
+        // });
+
         axios({
-            method: "post",
-            url: "http://localhost:8080/ZomatoProject_BackEnd/signup.php",
-            data: data,
-        }).then(function(response) {
-            console.log(response);
-            location.href = "../main_page.html";
-        });
+                method: "post",
+                url: "http://localhost:8080/ZomatoProject_BackEnd/signup.php",
+                data: vdata,
+            })
+            .then(function(response) {
+                console.log(response);
+                location.href = "../main_page.html";
+            })
+            .catch(function(response) {
+                //handle error
+                console.log(response);
+            });
     });
 }
