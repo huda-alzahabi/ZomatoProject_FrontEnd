5560aca823531f633673b80e8af6441fb83dbfbe
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-30 12:02:12 +0300

    Called restaurant from database and displayed it on screen

--- scripts/restaurant.js
+++ scripts/restaurant.js
@@ -1,5 +1,9 @@
 var ajax = new XMLHttpRequest();
-ajax.open("GET", "restaurant.php", true);
+ajax.open(
+    "GET",
+    "http://localhost:8080/ZomatoProject_BackEnd/restaurant.php",
+    true
+);
 ajax.send();
 
 ajax.onreadystatechange = function() {
@@ -14,3 +18,16 @@ ajax.onreadystatechange = function() {
         }
     }
 };
+// const btn = document.querySelector("button");
+// const post = document.querySelector(".post");
+// const widget = document.querySelector(".star-widget");
+// const editBtn = document.querySelector(".edit");
+// btn.onclick = () => {
+//     widget.style.display = "none";
+//     post.style.display = "block";
+//     editBtn.onclick = () => {
+//         widget.style.display = "block";
+//         post.style.display = "none";
+//     };
+//     return false;
+// };
