39edcf280aa59904d02c85679fcaecdd23cbd3b6
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-30 17:40:18 +0300

    Cleaned the code and added comments to login/signup script

--- scripts/login_signup.js
+++ scripts/login_signup.js
@@ -1,35 +1,41 @@
+/*Check whether the user wants to signup or login and act accordingly*/
 function loginSignup() {
     document.getElementById("signup").addEventListener("click", showSignUp);
     document.getElementById("login").addEventListener("click", loginToMain);
 }
 
+/*If the user clicks on signup*/
 function showSignUp() {
+    /*Defining variables*/
     let loginbox = document.getElementById("loginBox");
     let signupbox = document.getElementById("signupBox");
     let backbtn = document.getElementById("back");
     let submit_signup = document.getElementById("s");
+
+    /*If clicked on signup, go to signup box and hide login box*/
     loginbox.style.display = "none";
     signupbox.style.display = "block";
     backbtn.addEventListener("click", function() {
         signupbox.style.display = "none";
         loginbox.style.display = "block";
     });
-
+    /*When the user fills the values and submits, send the values to the url to save them in the db*/
     submit_signup.addEventListener("click", function() {
-        console.log(document.getElementById("full_name").value);
-        console.log(document.getElementById("signup_email").value);
-        console.log(document.getElementById("signup_pass").value);
-        console.log("Pressed");
+        /*Defining Variables*/
+        let full_name = document.getElementById("full_name").value;
+        let signup_email = document.getElementById("signup_email").value;
+        let signup_pass = document.getElementById("signup_pass").value;
 
-        let vdata = new FormData();
-        vdata.append("full_name", document.getElementById("full_name").value);
-        vdata.append("signup_email", document.getElementById("signup_email").value);
-        vdata.append("signup_pass", document.getElementById("signup_pass").value);
+        /*Append the variables set by the user to the Form Data to send them to url*/
+        let data = new FormData();
+        data.append("full_name", full_name);
+        data.append("signup_email", signup_email);
+        data.append("signup_pass", signup_pass);
 
         axios({
                 method: "post",
                 url: "http://localhost:8080/ZomatoProject_BackEnd/signup.php",
-                data: vdata,
+                data: data,
             })
             .then(function(response) {
                 console.log(response);
@@ -41,11 +47,16 @@ function showSignUp() {
     });
 }
 
+/*If the user clicks on login*/
 function loginToMain() {
+    /*Defining variables*/
+    let login_email = document.getElementById("login_email").value;
+    let login_pass = document.getElementById("login_pass").value;
     let data = new FormData();
-    data.append("login_email", document.getElementById("login_email").value);
-    data.append("login_pass", document.getElementById("login_pass").value);
-    console.log(data);
+
+    /*Append the variables set by the user to the Form Data to send them to url*/
+    data.append("login_email", login_email);
+    data.append("login_pass", login_pass);
     axios({
             method: "post",
             url: "http://localhost:8080/ZomatoProject_BackEnd/login.php",
