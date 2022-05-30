d8ff0d099fc407b81f26592d4eaa005b7f8fe95a
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-30 23:47:11 +0300

    Added comments to admins api

--- scripts/admin_apis.js
+++ scripts/admin_apis.js
@@ -1,9 +1,14 @@
 function addRestaurants() {
+    /*If the user clicks on add restaurant*/
     let add_rest = document.getElementById("submit");
     add_rest.addEventListener("click", function(e) {
         e.preventDefault();
+
+        /*Declaring variables*/
         let name = document.getElementById("name").value;
         let desc = document.getElementById("description").value;
+
+        /*Append the variables set by the user to the Form Data to send them to url*/
         let data = new FormData();
         data.append("name", name);
         data.append("description", desc);
