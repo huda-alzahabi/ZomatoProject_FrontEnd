f3c7c0c03cb1c5a108f15ea596098175e54057c6
Moe Tahan <mohamadtahan880@gmail.com>
2022-05-31 14:38:56 +0300

    Added view users page and styled it

--- scripts/admin_apis.js
+++ scripts/admin_apis.js
@@ -1,18 +1,4 @@
-//Styling Dropdwon
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
-}
 
 //addRestaurants() Allows the admin to add resaturants
 function addRestaurants() {
