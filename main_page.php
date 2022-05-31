982e6ee537eda192c0d46b28fcb5ebf7283ace44
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-31 20:19:26 +0300

    Included axios in main

--- main_page.php
+++ main_page.php
@@ -103,6 +103,8 @@
         </div>
     </section>
       <?php include "./footer.html" ?>
+     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="./scripts/main.js"></script>
 
 </body>
