2db740409bd80ccafbfc5ec5f730e7c6ba886314
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-31 15:56:54 +0300

    Fixed box sizes in login and edit profile

--- edit_profile.php
+++ edit_profile.php
@@ -6,20 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
-    <link rel="stylesheet" href="styles/edit.css">
+    <link rel="stylesheet" href="./styles/edit.css">
 </head>
 
 <body>
-        <div class="editBox" id="editBox"> <img class="userImg" src="./assets/pizza1.png" height="100px" width="100px">
-        <h2>EDIT PROFILE</h2>
-        <form>
-            <div class="inputBox">
-                <input id="fullname" type="text" name="fullname" placeholder="Full Name">
-                <input id="login_email" type="text" name="Email" placeholder="Email">
+        <div class="editBox" id="editBox">
+            <img class="userImg" src="./assets/pizza1.png" height="100px" width="100px">
+            <h2>EDIT PROFILE</h2>
+          <div name="login" action="login.php" method="post">
 
-            </div>
-            <button class="update">Update</button>
-        </form>
+                <div class="inputBox">
+                    <input id="fullname" type="text" name="fullname" placeholder="Full Name">
+                    <input id="login_email" type="text" name="Email" placeholder="Email">
+
+                </div>
+                <button class="update">Update</button>
+              </div>
+
+        </div>
         <script src="./scripts/edit_profile.js"></script>
 </body>
 
